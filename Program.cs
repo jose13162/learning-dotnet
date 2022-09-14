592fef219d99e692647351c8671db6deae469c95
jose13162 <anselmojose297@gmail.com>
2022-09-14 19:40:20 -0300

    Constructors

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet {
+  class Program {
+    static void Main() {
+      Book book = new Book("Harry Potter", "JK Rowling", 400);
+      Console.WriteLine(book.title);
+      Console.WriteLine(book.author);
+      Console.WriteLine(book.pages);
+    }
+  }
+}
