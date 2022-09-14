c13785e40071bbc820467a4c8cb7d5f91823786d
jose13162 <anselmojose297@gmail.com>
2022-09-14 12:29:13 -0300

    Classes and objects

---
+++ classes-and-objects/Book.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    public class Book
+    {
+        public string title = "";
+        public string author = "";
+        public int pages = 0;
+    }
+}
---
+++ classes-and-objects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classes_and_objects {
+  class Program {
+    static void Main() {
+      Book book1 = new Book();
+      book1.title = "Harry Potter";
+      book1.author = "JK Rowling";
+      book1.pages = 400;
+
+      Book book2 = new Book();
+      book2.title = "Lord Of the Rings";
+      book2.author = "Tolkein";
+      book2.pages = 700;
+
+      Console.WriteLine(book1.title);
+      Console.WriteLine(book2.author);
+    }
+  }
+}
