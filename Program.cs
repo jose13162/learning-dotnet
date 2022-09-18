16ea210e74d45139667512680886f32c16682f9d
jose13162 <anselmojose297@gmail.com>
2022-09-18 17:21:31 -0300

    Generics

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.IO;
 
 namespace dotnet {
   class Program {
     static void Main() {
-      string filePath = "assets/file.backup.txt";
+      Knight knight = new Knight();
+      Game<Knight> game = new Game<Knight>(knight);
 
-      try {
-        File.Delete(filePath);
-      } catch (Exception exception) {
-        Console.WriteLine(exception.Message);
-      }
+      game.warrior.Attack();
+    }
+  }
+
+  public interface IWarrior {
+    void Attack();
+  }
+
+  public class Knight : IWarrior {
+    public void Attack() {
+      Console.WriteLine("Knight attacks");
+    }
+  }
+
+  public class Game<T> where T : IWarrior {
+    public T warrior;
+
+    public Game(T warrior) {
+      this.warrior = warrior;
     }
   }
 }
