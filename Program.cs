e61e3c8c217e52f5921f5ec636dcfeee9aaf0c8f
jose13162 <anselmojose297@gmail.com>
2022-09-18 21:03:50 -0300

    Lambda expression

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ namespace dotnet {
   class Program {
     static void Main() {
       List<int> list = new List<int>();
-      int[] numbers = { 5, 6, 2, 4, 8, 1, 9, 3, 7, 10 };
+      int[] numbers = { 5, 6, 2, 4, 8, 1, 9, 3, 7 };
 
       list.AddRange(numbers);
-      Console.WriteLine(list.TrueForAll(LessThanTen));
-    }
+      bool isTrueForAll = list.TrueForAll((number) => {
+        return number < 10;
+      });
 
-    static bool LessThanTen(int number) {
-      return number < 10;
+      Console.WriteLine(isTrueForAll);
     }
   }
 }
